0aed3376e574145260a142a97f900270ca5ca229
AbhiChirme <abhishek.chirme11@gmail.com>
2023-12-11 23:45:12 +0530

    password generator

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,35 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useCallback , useEffect, useRef} from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [password, setPassword] = useState();
+  const [length, setLength] = useState(6);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charactersAllowed, setCharactersAllowed] = useState(false);
+
+  const passwordRef = useRef(null);
+
+  const passwordGenerator = useCallback(()=>{
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" ;
+
+    if(numberAllowed) str+= "0123456789"
+    if(charactersAllowed) str+="`~!@#$%^&*(){}[]"
+    for(let i=0 ; i<length ; i++){
+      let charIndex = Math.floor(Math.random()*str.length + 1);
+      pass += str.charAt(charIndex);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, charactersAllowed, setPassword])
+
+  const copyToClipboard = () => {
+    passwordRef.current?.select();
+    window.navigator.clipboard.writeText(password);
+  }
+  useEffect(()=>{
+    passwordGenerator();
+  },[length, numberAllowed, charactersAllowed,passwordGenerator])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-screen h-screen bg-black">
+      <div className="flex fixed justify-center inset-x-0 flex-wrap">
+        <div className="flex flex-wrap justify-center w-fit h-fit mx-auto my-10 bg-gray-700 rounded-lg">
+          <div className="justify-center px-5 py-5">
+            <div className="flex flex-wrap justify-center">
+            <h2 className="text-white text-4xl font-bold justify-center">
+              Password Generator
+            </h2>
+            </div>
+            <div className="flex flex-wrap px-5 py-5 justify-center w-full">
+              <input
+                type="text"
+                placeholder="Password"
+                readOnly
+                className="px-3 py-3  outline-none rounded-xl text-start w-200 text-white bg-black"
+                value={password}
+                ref={passwordRef}
+              ></input>
+              <button className="bg-blue-700 text-white rounded-lg  px-3 py-3" onClick={()=> copyToClipboard()}>
+                Copy
+              </button>
+            </div>
+            <div className="flex flex-wrap px-5 py-5 items-center gap-3">
+              <div className="flex items-center gap-1">
+                <input
+                  type="range"
+                  min={6}
+                  max={100}
+                  value={length}
+                  placeholder="Password"
+                  className="px-3 py-3  outline-none rounded-xl text-start text-white bg-black"
+                  onChange={(e) => {
+                    setLength(e.target.value);
+                  }}
+                ></input>
+                <label className="text-white"> Length {length}</label>
+              </div>
+              <div className="flex flex-wrap items-center gap-1">
+                <input
+                  type="checkbox"
+                  onChange={() => setNumberAllowed((prev) => !prev)}
+                ></input>
+                <lable className="text-white">Numbers</lable>
+              </div>
+              <div className="flex flex-wrap items-center gap-1">
+                <input
+                  type="checkbox"
+                  onChange={() => setCharactersAllowed((prev) => !prev)}
+                ></input>
+                <lable className="text-white">Character</lable>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
